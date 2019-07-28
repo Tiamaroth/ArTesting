55eb2400709a8c2a07153e61eab9ea7c0118197c
unknown <nicoluccic@gmail.com>
2019-07-28 13:14:02 +0200

    First commit

---
+++ Assets/Vuforia/Scripts/VideoController.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer mVid;
+
+    public void PlayVideo(){
+        mVid.Play();
+    }
+
+    public void StopVideo(){
+        mVid.Stop();
+    }
+}
